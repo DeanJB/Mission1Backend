b168a52959d5452a79fbb8066ff5d82496b16ac2
Dean Barrow <deanjordanbarrow@gmail.com>
2025-03-07 14:25:28 +1300

    I believe the backend is handling the request from front end correctly & the Ai prediction is sending that info back to the front end to be displayed

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-require("dovenv").config();
+// Module imports
+require("dotenv").config();
 const express = require("express");
 const multer = require("multer");
 const axios = require("axios");
@@ -10,3 +11,30 @@ const PORT = process.env.PORT || 4000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Multer image upload
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+app.post("/", upload.single("image"), async (req, res) => {
+      if (!req.file) return res.status(400).json({ error: "No image has been uploaded" });
+      try {
+            const apiURL = process.env.CUSTOM_VISION_URL; // URL + Key in .env
+            const apiKey = process.env.CUSTOM_VISION_KEY;
+
+            const response = await axios.post(apiURL, req.file.buffer, {
+                  headers: {
+                        "Content-Type": "application/octet-stream",
+                        "Prediction-Key": apiKey,
+                  },
+            });
+
+            res.json({ prediction: response.data.predictions });
+      } catch (error) {
+            console.error("Error:", error);
+            res.status(500).json({ error: "Crystal ball said it failed" });
+      }
+});
+
+// Server
+app.listen(PORT, () => console.log(`Server running on port http://localhost:4000/`));
